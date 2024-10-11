b9cddeec5ebe081996378099858b0a8e1edd5c9d
shreypatelm <sp3453@njit.edu>
2024-10-10 20:42:45 -0400

    Delete app/commands/exit directory

--- app/commands/exit/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-from app.commands import Command
-
-class ExitCommand(Command):
-    def execute(self, *args):
-        print("Exiting the program...")
-        sys.exit(0)  # Gracefully exit the program
